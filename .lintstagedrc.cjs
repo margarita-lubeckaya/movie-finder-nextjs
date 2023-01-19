4810c07ebd33608076ed95a71c768f765120fb9c
margarita.lubeckaya <margarita.lubeckaya@gmail.com>
2023-01-19 16:58:42 +0300

    add husky

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,3 +1,10 @@
 module.exports = {
-  '**/*.(ts|tsx)': ['npm run lint:fix', 'npm run format'],
+    '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
+    '**/*.(ts|tsx|js)': [(filenames) =>
+        `next lint --fix --file ${filenames
+            .map((file) => file.split(process.cwd())[1])
+            .join(" --file ")}`, 'npm run format']
+    ,
+    '**/*.(md|json)': (filenames) =>
+        `npx prettier --write ${filenames.join(' ')}`,
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,6 @@ import styles from '../styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const test = 'dddd'
-
   return (
     <>
       <Head>
